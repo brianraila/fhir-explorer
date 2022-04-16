b9579717886eaeb76912fb6e40de23bff3d497d3
Amolo <railamolo@gmail.com>
2022-04-16 15:54:13 +0000

    new fields: base_url, resource, id

--- src/App.js
+++ src/App.js
@@ -3,20 +3,41 @@ import './style.css';
 import { FHIRClient } from '@amolo/js-fhir';
 
 export default function App() {
-  let client = new FHIRClient('https://hapi.fhir.org/baseR4');
   let [data, setData] = useState(null);
-  let [id, setId] = useState('1');
-  let getData = async (id) => {
-    let response = await client.read('Patient', id);
+  let [id, setId] = useState('');
+  let [fhirBase, setFhirBase] = useState('')
+  let [resource, setResource] = useState('');
+
+  let getData = async (base, resource, id) => {
+    let client = new FHIRClient(base);
+    let response = await client.read(resource, id);
     console.log(response.content);
     setData(response.content);
   };
   useEffect(() => {
-    getData(id);
-  }, [id]);
+    getData(fhirBase, resource, id);
+  }, [id, resource, fhirBase]);
   return (
     <div>
-      <h1>FHIR Patient Resource Explorer.</h1>
+      <h1>FHIR Resource Explorer.</h1>
+      <input
+        style={{ borderRadius: '5px', height: '50px', width: '300px' }}
+        type="text"
+        placeholder="FHIR Base URL"
+        onChange={(e) => {
+          console.log(e.target.value);
+          setFhirBase(e.target.value);
+        }}
+      />
+      <input
+        style={{ borderRadius: '5px', height: '50px', width: '300px' }}
+        type="text"
+        placeholder="Resource"
+        onChange={(e) => {
+          console.log(e.target.value);
+          setResource(e.target.value);
+        }}
+      />
       <input
         style={{ borderRadius: '5px', height: '50px', width: '300px' }}
         type="text"
@@ -26,6 +47,7 @@ export default function App() {
           setId(e.target.value);
         }}
       />
+       
       <br />
       <br />
       <div
