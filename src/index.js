aca193874cac0f2cec321fd19fbb58baeff4c17a
Amolo <railamolo@gmail.com>
2022-04-21 20:06:51 +0000

    support for Chakra UI added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
+import _App from "./App";
 
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from '@chakra-ui/react'
+
+function App() {
+  // 2. Wrap ChakraProvider at the root of your app
+  return (
+    <ChakraProvider>
+      <_App />
+    </ChakraProvider>
+  )
+}
 ReactDOM.render(<App />, document.getElementById("root"));
